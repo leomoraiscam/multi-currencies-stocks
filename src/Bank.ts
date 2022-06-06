eb477f83e41c1a43a8b3f6a1db5fb4d86254587a
lmorais <leozonnn@gmail.com>
2022-06-06 17:09:09 -0300

    chore: revert classes

--- src/Bank.ts
+++
@@ -1,8 +0,0 @@
-import { Expression } from './expression';
-import { Money } from './Money';
-
-export class Bank {
-  reduce(source: Expression, to: string): Money {
-    return Money.dollar(10);
-  }
-}
