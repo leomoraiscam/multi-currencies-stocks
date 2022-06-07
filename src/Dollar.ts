38b5a58f636f8b86ddd7002aee7a3113f6bf41fa
lmorais <leozonnn@gmail.com>
2022-06-06 23:59:39 -0300

    feat: dollar equality

--- src/Dollar.ts
+++ src/Dollar.ts
@@ -9,6 +9,10 @@ class Dollar {
   times(multiplier: number): Dollar {
     return new Dollar((this.amount *= multiplier));
   }
+
+  equals(other: Dollar): boolean {
+    return this.amount === other.amount;
+  }
 }
 
 export default Dollar;
