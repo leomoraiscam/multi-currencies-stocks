a1fd8abc1dcb36250785595572c8a162586084cc
lmorais <leozonnn@gmail.com>
2022-06-07 14:14:35 -0300

    test: compare euros with dollars

--- src/money.spec.ts
+++ src/money.spec.ts
@@ -2,7 +2,7 @@ import Dollar from './Dollar';
 import Euro from './Euro';
 
 describe('Dollar', () => {
-  it('Should return 10 dollars when five dollars are multiplied by 2 and 15 when multiplier by 3', () => {
+  it('Should correctly handle dollar multiplication', () => {
     const five: Dollar = new Dollar(5);
 
     expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
@@ -10,7 +10,7 @@ describe('Dollar', () => {
     expect(five.times(3).equals(new Dollar(30))).toBeTruthy();
   });
 
-  it('Should return 10 euros when five euros are multiplied by 2 and 15 when multiplier by 3', () => {
+  it('Should correctly handle euro multiplication', () => {
     const five: Euro = new Euro(5);
 
     expect(five.times(2).equals(new Euro(10))).toBeTruthy();
@@ -18,11 +18,13 @@ describe('Dollar', () => {
     expect(five.times(3).equals(new Euro(30))).toBeTruthy();
   });
 
-  it('Should return true when dollars and euros of the same value are compared', () => {
+  it('Should correctly handle money equality', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
 
     expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
     expect(new Euro(5).equals(new Euro(6))).toBeFalsy();
+
+    expect(new Euro(5).equals(new Dollar(5))).toBeFalsy();
   });
 });
