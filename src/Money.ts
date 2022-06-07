d6eff8a654d668db407aadf8a5c23ea73043cd4b
lmorais <leozonnn@gmail.com>
2022-06-07 14:36:06 -0300

    feat: create factory methods for dollars and euros

--- src/Money.ts
+++ src/Money.ts
@@ -1,8 +1,19 @@
-class Money {
+/* eslint-disable no-return-assign */
+/* eslint-disable max-classes-per-file */
+
+export abstract class Money {
   public amount: number;
 
   constructor(amount: number) {
     this.amount = amount;
+  }
+
+  static dollar(amount: number): Dollar {
+    return new Dollar(amount);
+  }
+
+  static euro(amount: number): Euro {
+    return new Euro(amount);
   }
 
   equals(other: Money): boolean {
@@ -12,6 +23,28 @@ class Money {
 
     return this.amount === other.amount;
   }
+
+  abstract times(multiplier: number): Money;
 }
 
-export default Money;
+export class Dollar extends Money {
+  constructor(amount: number) {
+    super(amount);
+    this.amount = amount;
+  }
+
+  times(multiplier: number): Dollar {
+    return new Dollar((this.amount *= multiplier));
+  }
+}
+
+export class Euro extends Money {
+  constructor(amount: number) {
+    super(amount);
+    this.amount = amount;
+  }
+
+  times(multiplier: number): Euro {
+    return new Euro((this.amount *= multiplier));
+  }
+}
