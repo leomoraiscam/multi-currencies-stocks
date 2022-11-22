a7bf07b727afc93d1fc958635bc5305bb25c4180
lmorais <lmorais.contato@gmail.com>
2022-11-21 23:12:30 -0300

    refactor: get closer to abstaction

--- src/expression.ts
+++ src/expression.ts
@@ -6,4 +6,5 @@ import Money from './Money';
 /* eslint-disable @typescript-eslint/naming-convention */
 export interface Expression {
   reduce: (bank: Bank, to: string) => Money;
+  plus: (addend: Expression) => Expression;
 }
