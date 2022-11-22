7e5bbbc853cd4de56276b8df4485d3741b842267
lmorais <lmorais.contato@gmail.com>
2022-11-21 23:26:38 -0300

    feat: implementation missing operations

--- src/expression.ts
+++ src/expression.ts
@@ -7,4 +7,5 @@ import Money from './Money';
 export interface Expression {
   reduce: (bank: Bank, to: string) => Money;
   plus: (addend: Expression) => Expression;
+  times: (multiplier: number) => Expression;
 }
